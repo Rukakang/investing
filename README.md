e52d7bdd52a87d79b676e095f4e084a5a3ccb41e
jiebao <908822571@qq.com>
2023-04-30 18:21:56 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,29 @@
+<b>quantitative investing</b>
+## 前序准备
+
+**运行前准备：**
+
+由于此项目是基于nodejs的前后端结合项目，你需要进行nodejs的相关准备工作。项目运行之前，请确保系统已经安装以下应用：
+
+(1)、node (6.0 及以上版本)。使用细节，请参考：[node的下载及安装。](https://nodejs.org/en/download/)
+
+## 开发：
+1.  拉取代码后运行 npm install
+2.  运行npm run serve
+
+**本地运行：**
+
+**npm run serve** 运行之后，会默认打开本地访问路径
+
+**发布：**
+
+**npm run bulid** (生成打包之后的项目文件,此文件主要用于项目部署)。
+
+**温馨提示**：
+
+1、项目图标来源：
+本项目图标线上地址为：//at.alicdn.com/t/font\_1258069\_e40c6mwl0x8.js， 在public/index.html中引入;
+
+2、执行npm run dev报错处理方案：
+可能是你的本机安装nodejs版本过高，请卸载nodejs高版本，重新安装较低版本(如v10.9.0)；
+或者通过安装nvm来切换到较低版本也可以实现正常启动；
---
+++ src/router/index.js
@@ -0,0 +1,187 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import {Layout, Content} from "../layout"; // 页面整体布局
+import {topRouterMap} from "./topRouter";
+
+Vue.use(Router);
+
+function filterTopRouterMap(name) {
+    let router = topRouterMap.find((item) => {
+        return item.parentName === name;
+    });
+    return router.data; // arr
+}
+
+/**
+ * 1、roles:后台返回的权限结构;
+ *
+ */
+//手动跳转的页面白名单
+const whiteList = [
+    '/'
+];
+/**
+ * path:''与path:'*'的区别：
+ * 1、path:'*', 会匹配所有路径
+ * 2、path:''，也是会匹配到路由
+ *
+ */
+//默认不需要权限的页面
+export const constantRouterMap = [
+    {
+        path: '',
+        component: Layout,
+        redirect: '/index/index',
+        hidden: true
+    },
+    {path: '/login', name: 'login', component: () => import('@/page/login'), hidden: true},
+    {path: '/404', component: () => import('@/page/errorPage/404'), hidden: true},
+    {path: '/401', component: () => import('@/page/errorPage/401'), hidden: true},
+    {
+        path: '/index',
+        name: 'index',
+        component: Layout,
+        meta: {
+            title: '首页',
+            icon: 'icondashboard',
+        },
+        noDropdown: true,
+        children: [
+            {
+                path: 'index',
+                meta: {
+                    title: '首页',
+                    icon: 'icondashboard',
+                    routerType: 'leftmenu'
+                },
+                component: () => import('@/page/index/index'),
+            }
+        ]
+    }
+]
+
+//注册路由
+export default new Router({
+    mode: 'history', // 默认为'hash'模式
+    base: '/permission/', // 添加跟目录,对应服务器部署子目录
+    routes: constantRouterMap
+})
+
+//异步路由（需要权限的页面）
+export const asyncRouterMap = [
+    {
+        path: '/infoManage',
+        name: 'infoManage',
+        meta: {
+            title: '信息管理',
+            icon: 'iconinfo',
+        },
+        component: Layout,
+        children: [
+            {
+                path: 'infoShow',
+                name: 'infoShow',
+                meta: {
+                    title: '个人信息',
+                    icon: 'iconinfo',
+                    routerType: 'leftmenu',
+                    titleList: [
+                        {"path": "infoShow1", "title": "个人信息子菜单1"},
+                        {"path": "infoShow2", "title": "个人信息子菜单2"},
+                        {"path": "infoShow3", "title": "个人信息子菜单3"},
+                        {"path": "infoShow4", "title": "个人信息子菜单4"},
+                        {"path": "infoShow5", "title": "个人信息子菜单5"}
+                    ]
+                },
+                component: Content,
+                children: filterTopRouterMap('infoShow')
+            },
+        ]
+    },
+    {
+        path: '/fundManage',
+        name: 'fundManage',
+        meta: {
+            title: '资金管理',
+            icon: 'iconpay3',
+        },
+        component: Layout,
+        children: [
+            {
+                path: 'fundList',
+                name: 'fundList',
+                meta: {
+                    title: '资金流水',
+                    routerType: 'leftmenu'
+                },
+                component: () => import('@/page/fundList/fundList'),
+            },
+            {
+                path: 'chinaTabsList',
+                name: 'chinaTabsList',
+                meta: {
+                    title: '区域投资',
+                    routerType: 'leftmenu'
+                },
+                component: () => import('@/page/fundList/chinaTabsList'),
+            }
+        ]
+    },
+    {
+        path: '/fundData',
+        name: 'fundData',
+        meta: {
+            title: '资金数据',
+            icon: 'iconecharts',
+        },
+        component: Layout,
+        redirect: '/fundData/fundPosition',
+        children: [
+            {
+                path: 'fundPosition',
+                name: 'fundPosition',
+                meta: {
+                    title: '投资分布'
+                },
+                component: () => import('@/page/fundData/fundPosition')
+            },
+            {
+                path: 'typePosition',
+                name: 'typePosition',
+                meta: {
+                    title: '项目分布'
+                },
+                component: () => import('@/page/fundData/typePosition')
+            },
+            {
+                path: 'incomePayPosition',
+                name: 'incomePayPosition',
+                meta: {
+                    title: '收支统计'
+                },
+                component: () => import('@/page/fundData/incomePayPosition')
+            }
+        ]
+    },
+    {path: '*', redirect: '/404', hidden: true}
+];
+
+/**
+ *  路由设置要求：
+ * 1、该路由有子菜单,可以设置多层嵌套路由children;如果没有子菜单,不需要设置children;通过item.children.length来判断路由的级数;
+ * 2、登录成功后,定位到系统首页时,需要加载页面整体布局组件Layout并进行子路由定向加载;
+ *
+ * 按需加载路由组件的2种方法：
+ * 1、component: () => import('@/page/login')
+ * 2、component:resolve => require(['@/page/fundPosition'], resolve)
+ *
+ *
+ *
+ * 什么情况下，路由会定位到404页面?
+ * 路由中redirect:'',不起作用？
+ * 三级子菜单要在顶部展示？
+ *
+ *
+ *
+ */
+
